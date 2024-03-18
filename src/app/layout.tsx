64b5169dcb66bb6cff09a7fe7c1bdf623b313fc4
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-03-18 13:55:10 +0100

    Added basic structure, tailwind colors config & vitest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main>{children}</main></body>
     </html>
   );
 }
