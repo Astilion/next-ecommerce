1d5540b27c05288157204ed79a7a74a66c4bbba4
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-03-19 08:39:10 +0100

    added useScreenDetector hook

---
+++ src/hooks/useScreenDetector.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+export default function useScreenDetector() {
+    const [width, setWidth] = useState(window.innerWidth);
+    const handleWindowSizeChange = () => {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleWindowSizeChange);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowSizeChange);
+        };
+    }, []);
+
+    const isMobile = width <= 768;
+    const isDesktop = width > 1024;
+
+    return { isMobile, isDesktop };
+}
