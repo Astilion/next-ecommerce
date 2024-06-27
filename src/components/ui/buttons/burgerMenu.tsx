d4dc7005a25f5e4a9adec9ead35966532d3dfdb9
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-06-27 11:38:21 +0200

    fixed cross nav

--- src/components/ui/buttons/burgerMenu.tsx
+++ src/components/ui/buttons/burgerMenu.tsx
@@ -6,20 +6,20 @@ interface BurgerMenuProps {
 const BurgerMenu = ({ isNavOpen, toggleNav }: BurgerMenuProps) => {
     return (
         <button
-            className={`group mr-auto p-3 group ${isNavOpen ? styles.cross : styles.burger}`}
+            className={` flex justify-center group relative mr-auto group p-3 ${isNavOpen ? styles.cross : styles.burger}`}
             onClick={toggleNav}
         >
-            <div
-                className={` bg-creme-500 group-hover:bg-accent-500 transition duration-300 ${styles.line} ${styles.burger} ${isNavOpen ? "" : styles.opacity1}`}
-            ></div>
-            <div
-                className={`bg-creme-500 group-hover:bg-accent-500 transition duration-300 ${styles.line} ${styles.cross} ${isNavOpen ? styles.rotateMinus45 : styles.opacity0
+            <span
+                className={`absolute bg-creme-500 group-hover:bg-accent-500 transition duration-300 ${styles.line} ${styles.burger} ${isNavOpen ? "" : styles.opacity1}`}
+            ></span>
+            <span
+                className={`absolute bg-creme-500 group-hover:bg-accent-500 transition duration-300 ${styles.line} ${styles.cross} ${isNavOpen ? styles.rotateMinus45 : styles.opacity0
                     }`}
-            ></div>
-            <div
-                className={`bg-creme-500 group-hover:bg-accent-500 transition duration-300 ${styles.line} ${styles.cross} ${isNavOpen ? styles.rotate45 : styles.opacity0
+            ></span>
+            <span
+                className={`absolute bg-creme-500 group-hover:bg-accent-500 transition duration-300 ${styles.line} ${styles.cross} ${isNavOpen ? styles.rotate45 : styles.opacity0
                     }`}
-            ></div>
+            ></span>
         </button>
     );
 };
